be02f6f057641ecae73c137b13915520f959ec62
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-17 17:01:46 +0200

    feat(tags): add tags table

---
+++ app/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasUuids;
+
+    protected $table = "tags";
+    protected $keyType = "string";
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name'
+    ];
+}
