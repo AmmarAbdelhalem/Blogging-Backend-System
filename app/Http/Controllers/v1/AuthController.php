332d6b40bbf5195f076a7a7eae960fa3d1fef37f
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-18 14:14:53 +0200

    feat(auth): add login, me, logout logic to AuthController

--- app/Http/Controllers/v1/AuthController.php
+++ app/Http/Controllers/v1/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,7 +11,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    // TODO login, logout, me, email_verification, 2fa, password reset
+    // TODO email_verification, 2fa, password reset
     public function register(RegisterRequest $request) {
         $user = User::create([
             "name" => $request->name,
@@ -26,4 +27,32 @@ class AuthController extends Controller
         ], 201);
     }
 
+    public function login(LoginRequest $request) {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || Hash::check($request->password, $user->password)) {
+            return response()->json([
+                "message" => "invalid credentials"
+            ], 401);
+        }
+
+        $token = $user->createToken("auth_token")->plainTextToken;
+
+        return response()->json([
+            "user" => $user,
+            "token" => $token
+        ]);
+    }
+
+    public function me(Request $request) {
+        return response()->json($request->user());
+    }
+
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
 }
