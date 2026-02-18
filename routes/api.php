0ef058da79f63934aa117eab68e2037a7d6c359d
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-18 14:15:44 +0200

    feat(api): add login, me, logout endpoints

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,25 @@ use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(ForceJsonResponse::class)->group(function () {
-    Route::prefix('v1')->group(function () {
-        Route::post('/register', [AuthController::class, 'register']);
+// Route::middleware(ForceJsonResponse::class)->group(function () {
+//     Route::prefix('v1')->group(function () {
+//         Route::post('/register', [AuthController::class, 'register']);
+//         Route::post('/login', [AuthController::class, 'login']);
+
+//         Route::middleware("auth:sanctum")->group(function () {
+//             Route::get('/me', [AuthController::class, 'me']);
+//         });
+//     });
+// });
+
+Route::prefix("v1")->group(function () {
+    Route::middleware(ForceJsonResponse::class)->group(function () {
+        Route::post("/register", [AuthController::class,"register"]);
+        Route::post("/login", [AuthController::class,"login"]);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('/me', [AuthController::class,'me']);
+            Route::get('/logout', [AuthController::class,'logout']);
+        });
     });
 });
